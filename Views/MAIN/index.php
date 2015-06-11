20ede525f60f864af8acf9dce3707fcacb7941d7
Franksoft24 <frankmer95@gmail.com>
2015-06-11 15:53:29 -0400

    ...

--- Views/MAIN/index.php
+++ Views/MAIN/index.php
@@ -2,7 +2,7 @@
 	
 	<!--div class='text-contener'>Welcome</div-->
 	<div class='img-center-round blue'><img src="../../resource/img/logo.png" width="100%" alt="logo.png"></div>
-	<div class='text-contener'> Hi, my name is frankmer<br> software engineer </div>
+	<div class='text-contener'> Hi, my name is frankmer<br> I'm software engineer </div>
 	<div class="nav-button-next"><a href="../about/"><img src="../../resource/img/next.png" alt="next.png" width="100%"></a></div>
 	<script type="text/javascript">
 		headerTitle("Welcome");
